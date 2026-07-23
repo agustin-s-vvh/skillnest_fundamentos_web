89d37032b184fa903aefe6e20e0727d7990354b6
jeremiasgaete <jeremiasgaete@liceovvh.cl>
2026-07-23 11:58:57 -0400

    ok

--- STACK_JS/07_Arreglos/static/js/script.js
+++ STACK_JS/07_Arreglos/static/js/script.js
@@ -1,61 +1,134 @@
 console.log("Conexión con js exitosa...")
 
-// Variable a lista
+function Nombres() {
 
-let nombre1 = "Ana";
-let nombre2 = "Pedro";
-let nombre3 = "María";
-let nombre4 = "Carlos";
+    let nombre1 = "Ana";
+    let nombre2 = "Pedro";
+    let nombre3 = "María";
+    let nombre4 = "Carlos";
 
-// Transofrmar en array y mostrar 
+    // Transformar en array y mostrar
 
-let Nombres = ["Ana", "Pedro", "María", "Carlos"]
+    let Nombres = ["Ana", "Pedro", "María", "Carlos"]
 
-// Mostrar a María en una alerta
-console.log(nombres[2]); // Muestra a María
-let compras = [5000, 2300, 12000, 4500, 3000];
-let alumnos = ["Ana", "Pedro", "María", "Carlos"];
-let datos = ["Carlos", 18, true];
-let frutas = ["Manzana", "Pera", "Kiwi", "Sandía"];
+    //Mostrar a María en una alerta
+    alert(Nombres[2]);
+}
 
-// Desafío formar una oracióm con elementos
-// de las distintas variables
-// Pedro tiene 18 años, y compró una sandía a $2300
-let mensaje = `${alumnos[1]} tiene ${datos[1]} años, y compró una ${frutas[3]} a $${compras[2]}.`;
-alert(mensaje)
+function ComprasdeFrutas() {
 
-function Frutas(){
-let frutas = ["Manzana", "Pera", "Kiwi", "Sandía"];
-alert(frutas.length);
-} 
+    let compras = [5000, 2300, 12000, 4500, 3000];
+    let alumnos = ["Ana", "Pedro", "María", "José"];
+    let datos = ["Carlos", 18, true];
+    let frutas = ["Manzana", "Pera", "Kiwi", "Sandía"];
 
+    let mensaje = `${alumnos[1]} tiene ${datos[1]} años, y compró una ${frutas[3]} a $${compras[2]}.`;
+    alert(mensaje)
+}
 
-let frutas = ["Manzana", "Pera", "Kiwi", "Sandía"];
-let ultima = frutas[frutas.length - 1]
-alert(ultima)
+function Frutas() {
+    let frutas = ["Manzana", "Pera", "Kiwi", "Sandía"];
+    alert(frutas.length);
+    let ultima = frutas[frutas.length - 1]
+    alert(ultima)
+}
+
 //variable.length -1 siempre trae el ultimo
 
 //Desafio
 //Del arreglo ["Hola", "a", "todos", "los", "Programadores!"]
 //Mostrar el penultimo elemento y el ultimo
 
-function Palabras(){
-let palabras =["Hola", "a", "todos", "los", "Programadores!"]
-let penultima = palabras[palabras.length - 2];
-let ultima = palabras[palabras.length - 1];
-alert(penultima + "" + ultima)
+function Palabras() {
+    let palabras = ["Hola", "a", "todos", "los", "Programadores!"]
+    let penultima = palabras[palabras.length - 2];
+    let ultima = palabras[palabras.length - 1];
+    alert(penultima + "" + ultima)
 }
 
-function ManipularArreglo(){
+function ManipularArreglo() {
     let listaTareas = ["Barrer", "Cocinar", "Realizar compras"]
     //  Modificar un elemento del arreglo
     listaTareas[1] = "Construir";
     //añadir un elemento al final de la lista
     listaTareas.push("Cocinar");
     //Eliminar el ultimo elemento y mostralo
+    let Eliminado = listaTareas.pop()
+    //Añadir un elemento al inicio de la lista
+    listaTareas.unshift("Boxear")
+    //eliminar el primer elemento de la lista
+    Eliminado += " - " + listaTareas.shift();
     //Mostar el resultado
     alert(listaTareas.join(" - "));
+    alert("Elementos eliminados: " + Eliminado)
 }
+
+
+function notas() {
+
+    let notas = [6.2, 5.8, 4.1, 7.0];
+    let suma = 0;
+
+    for (let i = 0; i < notas.length; i++)
+        suma += notas[i];
+    {
+
+        alert(`La suma acumulada es: ${suma}`);
+        let promedio = suma / notas.length;
+        alert(`El promedio de las notas: ${notas.join(" / ")}\nPromedio: ${promedio}`)
+    }
+}
+
+function Elementos() {
+    let ventas = [10000, 5000, 12000, 8000]
+    let total = 0;
+
+    for (let i = 0; i < ventas.length; i++) {
+        total += ventas[i]
+        alert(`El resultado final es: ${total}`)
+    }
+
+}
+// Condicional de bucles
+function edades() {
+    let edades = [12, 15, 18, 20, 25]
+    let mayores = []
+    for (let i = 0; i < edades.length; i++)
+        if (edades[i] >= 18) {
+            mayores.push(edades[i]);
+        }
+}
+alert(`De la lista de edades: ${edades.join(" / ")}
+\nLos mayores son:  `)
+
+
+function mayor() {
+    let numeros = [10, 35, 7, 90, 22];
+    let mayor = numeros[0];
+    for (let i = 1; i < numeros.length; i++) {
+        if (numeros[i] > mayor) {
+            mayor = numeros[i];
+        }
+
+    }
+}
+
+alert(mayor);
+
+function menor() {
+    let numeros = [10, 35, 7, 90, 22];
+    let menor = numeros[0];
+    let mayor = numeros[0];
+    for (let i = 1; i < numeros.length; i++) {
+        if (numeros[i] < menor) {
+            menor = numeros[i];
+        }
+    }
+}
+
+alert(menor);
+
+// # 💻 Ejercicios
 
 // ## Ejercicio 1
 
@@ -71,18 +144,31 @@ function ManipularArreglo(){
 // - Última edad.
 // - Cantidad de elementos.
 
-function edad(){
+function edad() {
     let edades = [15, 18, 20, 14, 25]
     let edad = edades[edades.length - 5];
     let edad1 = edades[edades.length - 1];
-    alert(edad + "/" + edad1 + "/" + edades.length)
+    alert(edad)
+    alert(edad1)
+    alert(edades.length)
+
 }
+// ---
 
 // ## Ejercicio 2
 
 // Crear un arreglo con cinco nombres.
 
 // Mostrar todos utilizando un ciclo `for`.
+
+function Orden() {
+    let Nombres = ["Ana", "Juan", "Alexander", "Jake"]
+    for (let i = 0; i < Nombres.length; i++) {
+        alert(i)
+    }
+}
+
+
 
 // ---
 
@@ -120,6 +206,10 @@ function edad(){
 
 // - Cuántos son pares.
 // - Cuántos son impares.
+
+let Numeros = ["2,3,4,5,6,7,8,9,10,11"]
+
+
 
 // ---
 
